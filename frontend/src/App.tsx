7a5a1db8a3eeb912dfa182fc4bc603aaca8d002b
hyeyoungkim <velvetredvelvet91@gmail.com>
2025-12-29 01:51:21 +0900

    [feature/Login] 로그인 페이지 UI 작성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,34 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+// import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:5050/api/users')
-      .then(response => response.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error('Error fetching users:', error));
-  }, []);
+  // useEffect(() => {
+  //   fetch('http://localhost:5050/api/users')
+  //     .then((response) => response.json())
+  //     .then((data) => setUsers(data))
+  //     .catch((error) => console.error('Error fetching users:', error));
+  // }, []);
 
   return (
     <>
-      {users.map((user: { id: string, name: string, email: string }) => (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login isLogin={false} />} />
+          <Route path="/signin" element={<Login isLogin={true} />} />
+        </Routes>
+      </Router>
+      {/* {users.map((user: { id: string; name: string; email: string }) => (
         <div key={user.id}>
           <h2>{user.name}</h2>
           <p>{user.email}</p>
         </div>
-      ))}
+      ))} */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
